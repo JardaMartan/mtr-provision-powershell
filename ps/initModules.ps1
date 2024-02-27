7c19f635d5755476e720d46b0d9b5c0c50fe46e5
Jaroslav Martan <jmartan@cisco.com>
2024-02-27 10:05:01 +0100

    initialize modules needed for the scripts to run

---
+++ ps/initModules.ps1
@@ -0,0 +1,7 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+[system.net.webrequest]::defaultwebproxy = new-object system.net.webproxy($PROXY_URL)
+#[system.net.webrequest]::defaultwebproxy.credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+[system.net.webrequest]::defaultwebproxy.BypassProxyOnLocal = $true
+
+Install-Module LINQ
+Add-Type -AssemblyName System.Web
